d14d1cb020a08cc97336bcb982ff402650356954
Syahmi Zulkepli <msyahmizulkepli@gmail.com>
2022-05-21 00:03:29 +0800

    Add files via upload

---
+++ src/components/Connection.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Connection extends Component {
+	render() {
+		return (
+				<div>
+                    <h1>About</h1>
+                    <p>This is a Fleet Management System for monitoring and controlling IIUM Medibots</p>
+				</div>
+		);
+	}
+}
+
+export default Connection;
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Connection from './Connection';
+
+class Home extends Component {
+    state = {};
+	render() {
+		return (
+				<div>
+                    <h1 className="text-center mt-3">Robot Control Page</h1>
+				</div>
+		);
+	}
+}
+
+export default Home;
