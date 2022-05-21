7ce88c71a11a352d54198232205bc142db3eb72d
Syhami Zulkepli <msyahmizulkepli@gmail.com>
2022-05-21 23:59:06 +0800

    added multirobot teleoperation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Connection from './Connection';
+import { Row , Col , Container } from "react-bootstrap";
+import Teleoperation from "./Teleoperation";
 
 class Home extends Component {
     state = {};
@@ -8,6 +10,7 @@ class Home extends Component {
 				<div>
                     <h1 className="text-center mt-3">Robot Control Page</h1>
 					<Connection/>
+					<Teleoperation/>
 				</div>
 		);
 	}
